d21952d8d023406897b81d1a979b1e760643c79b
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-28 12:16:10 +0200

    Exercice 5 G2

---
+++ ExG2/src/be/abis/courseadmin/test/ExF4.java
@@ -0,0 +1,37 @@
+package be.abis.courseadmin.test;
+
+import java.io.IOException;
+
+import be.abis.courseadmin.model.Course;
+import be.abis.courseadmin.model.Instructor;
+
+public class ExF4 {
+
+	public static void main(String[] args) {
+			
+		try {
+			Instructor instr1 = new Instructor("Cecile", "Navez", 46, 27, 3000.0);
+			instr1.printSalaryHistory();
+			
+			Instructor instr2 = new Instructor("Philippe", "Dupont", 50, 20, 1500.0);
+			instr2.printSalaryHistory();
+			
+			boolean priorknowledge = true;
+			Course myCourse = new Course("Java Training", 5, 735.30, priorknowledge);
+			myCourse.addInstructor(instr1);
+			myCourse.addInstructor(instr2);
+			
+			myCourse.printInfo();
+						
+			
+			System.out.println("Infos printed in files printing.txt and salaryhistory.txt" );
+		
+		} catch (NumberFormatException e) {
+			 System.out.println("Insert correct argument, try again");
+			 
+		}  catch (IOException ioe){
+			  System.out.println(ioe.getMessage());
+		 }
+		 
+	}
+}
