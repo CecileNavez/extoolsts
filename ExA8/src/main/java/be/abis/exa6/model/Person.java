56e203740581a47638000e20859ccac96f36afcd
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-29 12:47:07 +0200

    ExA8

---
+++ ExA8/src/main/java/be/abis/exa6/model/Person.java
@@ -0,0 +1,113 @@
+package be.abis.exa6.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+	
+	private int personNumber;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthDate;
+	private Company company;
+	
+	public Person(int personNumber, String firstName, String lastName, LocalDate birthDate) {
+		super();
+		this.personNumber = personNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+	}
+	
+	public Person(int personNumber, String firstName, String lastName, LocalDate birthDate, Company company) {
+		super();
+		this.personNumber = personNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+		this.company = company;
+	}
+	
+	
+
+	public int getPersonNumber() {
+		return personNumber;
+	}
+
+
+
+	public void setPersonNumber(int personNumber) {
+		this.personNumber = personNumber;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+
+	public Company getCompany() {
+		return company;
+	}
+
+
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+
+	@Override
+	public String toString(){
+		
+		int age = this.calculateAge();
+		
+		String text = "Person " + this.personNumber + " : " + this.firstName + " " + this.lastName + " (" + age + " years old)";
+		if (this.company != null) {
+			text += " works for " + this.company.getName() + " in " + this.company.getAddress().getTown() + ".";
+	}	else {
+		
+		text += " is unemployed for the moment.";
+	}
+		
+		return text;
+		
+	}
+	
+	public int calculateAge() {
+		int age = Period.between(birthDate, LocalDate.now()).getYears();	
+		return age;
+	}
+}
---
+++ ExA8/src/main/java/be/abis/exa6/test/ExA8.java
@@ -0,0 +1,36 @@
+package be.abis.exa6.test;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+import be.abis.exa6.model.Address;
+import be.abis.exa6.model.Company;
+import be.abis.exa6.model.Person;
+
+public class ExA8 {
+
+	public static void main(String[] args) {
+		
+		Address a1 = new Address("rue du marais", "10", "1000", "Brussels", "Belgium","BE");
+		Company c1 = new Company("BNPPF", a1);
+		Person p1 = new Person(1, "Cecile", "Navez", LocalDate.of(1974, 1, 3), c1);
+	
+		Person p2 = new Person(2, "Phil", "Dupont", LocalDate.of(1970, 2, 16) );
+		
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		
+		try (BufferedWriter bw = Files.newBufferedWriter(Paths.get("personinfo.txt"));) {
+			bw.write(p1.toString() + "\n");
+			bw.write(p2.toString() + "\n");
+			
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
